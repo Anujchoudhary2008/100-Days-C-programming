af1a4e8c46ac1a832760567b0ce8c8a88d47f89b
Anujchoudhary2008 <Anuj.32557@stu.upes.ac.in>
2026-08-17 07:36:05 +0530

    Add files via upload

---
+++ 02_day1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int a,b;
+    int sum,diff,product,quotient;
+    scanf("%d %d", &a ,&b);
+    sum = a+b;
+    diff = a-b;
+    product = a*b;
+    quotient= a/b;
+    printf("sum=%d, diff=%d,product=%d,quotient=%d", sum, diff,product,quotient);
+    return 0;
+
+
+}
